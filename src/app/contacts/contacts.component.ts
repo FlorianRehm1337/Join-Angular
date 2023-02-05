d24a02808f7deaa8241b82ebe7658a781d52be31
FlorianRehm1337 <florehm1337@gmail.com>
2023-02-05 16:48:20 +0100

    contact Design finished

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contacts',
@@ -8,12 +8,34 @@ import { Component, OnInit } from '@angular/core';
 export class ContactsComponent implements OnInit {
 
   constructor() { }
+  windowWidth: number = window.innerWidth;
 
+  @HostListener('window:resize') onResize(){
+
+    this.windowWidth = window.innerWidth;
+    if (this.windowWidth > 1000) {
+      this.isDesktopView = true;
+    } else {
+      this.isDesktopView = false;
+    }
+  }
+
+
+  detailView: boolean = false; //hide contactlist
+  isDesktopView: boolean = false;
   ngOnInit(): void {
   }
 
+  openDetailView(){
+    this.detailView = true;
+  }
+
+  closeDetailView(){
+    this.detailView = false;
+  }
+
   openAddContact(){
-    
+
   }
 
 }
